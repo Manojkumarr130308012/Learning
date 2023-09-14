c80d4202c5b644af242a5da4a9e0c50f430f0029
manojthoffen <manojkumar.t@hoffensoft.com>
2023-09-14 11:19:06 +0530

    updated cron job

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -25,7 +25,7 @@ const crypto = require('crypto');
 server.use(cors());
 
 
-cron.schedule('6 * * * * *', () =>  {
+cron.schedule('* 12 * * * *', () =>  {
     console.log('stopped task');
         var totalval = 0.0;
     	var dbf=admin.database();
