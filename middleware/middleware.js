899c9e8d7290b0aa5506d373c857f352505c9c54
manojthoffen <manojkumar.t@hoffensoft.com>
2023-09-14 17:01:20 +0530

    testing

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -25,7 +25,7 @@ const crypto = require('crypto');
 server.use(cors());
 
 
-cron.schedule('0 0 17 * * *', () =>  {
+cron.schedule('0 0 19 * * *', () =>  {
     console.log('stopped task');
         var totalval = 0.0;
     	var dbf=admin.database();
