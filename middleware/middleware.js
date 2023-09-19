89ea466a97b2f6ade54fa706862fbef9f4b716b1
manojthoffen <manojkumar.t@hoffensoft.com>
2023-09-19 17:02:43 +0530

    notification

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -213,6 +213,22 @@ server.use(cors());
                            adminRef.child('History').child(orderId).child('Bill').update(updatebill);
     
                            billingRef.child(userId).child(orderId).child('Bill').update(updatebill);
+
+
+                           const  registrationToken = "fjXKPGUqSoKNuiB_-4RMBF:APA91bFoVN-bdx9m21otiqeKCxSr-U2QbAZhbD_ouJMkxPzpUwmuI5bPG7CzqKA-BJ6Si5WdMfxZJV2r31Q5OlA2TWQYPD_A5GFLFaeo5nT63OBiKh8ATTiZd6qRErlphMQ41XgzGh4x"
+                           const message = "message sent sucess fully"
+                           const options =  notification_options
+                           
+                             admin.messaging().sendToDevice(registrationToken, message, options)
+                             .then( response => {
+                       
+                            //   res.status(200).send("Notification sent successfully")
+                              
+                             })
+                             .catch( error => {
+                                 console.log(error);
+                             });
+                       
                          
                         })
     
