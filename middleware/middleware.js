2758aaa9e9b8980df1db86886728e5f64c8553ec
manojthoffen <manojkumar.t@hoffensoft.com>
2023-09-19 09:43:11 +0530

    approced dynamicaly

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -117,6 +117,108 @@ cron.schedule('5 * * * * *', () =>  {
         scheduled: true,
         timezone: "Asia/Kolkata"
     });
+
+
+    server.put('/ApprovedDynamic', (req, res)=>{
+
+        try{
+        
+            var totalval = 0.0;
+            var dbf=admin.database();
+            var adminRef=dbf.ref("AdminData");
+            var billingRef=dbf.ref("Billing");
+    
+            adminRef.child('Billing').once('value').then(function(snapshot) {
+                snapshot.forEach((snapshot) => {
+    
+                     if(snapshot.child('Bill').child('orderstate').val() == "Pending"){
+                          var orderId = snapshot.child('Bill').child('OrderID').val();
+                          var userId = snapshot.child('Bill').child('userId').val();
+    
+    
+                       //adminData itemlist amount update 
+                        snapshot.child('ItemList').forEach((itemlist) => {
+                            const weight=itemlist.child('weight').val();
+                            var vegtableRef=dbf.ref("VegetableEntry/"+ itemlist.child('id').val());
+                            vegtableRef.once('value').then(function(vegtablelist){
+                                const Price=vegtablelist.child('Rate').val();
+                                const totalstr = Price * weight;
+                                totalval = totalval + totalstr;
+                                var item = {
+                                    rate : totalstr
+                                    };
+                                adminRef.child('Billing').child(orderId).child('ItemList').child(itemlist.key).update(item);
+                            })
+                         })
+    
+                         //admindata history  itemlist amount update
+                         adminRef.child('History').child(orderId).child('ItemList').once('value').then(function(snapshot) {
+                            snapshot.forEach((historyitem) => {
+                            const weight=historyitem.child('weight').val();
+                            var vegtableRef=dbf.ref("VegetableEntry/"+ historyitem.child('id').val());
+                            vegtableRef.once('value').then(function(vegtablelist){
+                                const Price=vegtablelist.child('Rate').val();
+                                const totalstr = Price * weight;
+                                totalval = totalval + totalstr;
+                                var item = {
+                                    rate : totalstr
+                                    };
+                                adminRef.child('History').child(orderId).child('ItemList').child(historyitem.key).update(item);
+                            })
+                         })
+                        })
+    
+    
+                        // consumer itemlist
+                        billingRef.child(userId).child(orderId).child('ItemList').once('value').then(function(snapshot) {
+                            snapshot.forEach((historyitem) => {
+                            const weight=historyitem.child('weight').val();
+                            var vegtableRef=dbf.ref("VegetableEntry/"+ historyitem.child('id').val());
+                            vegtableRef.once('value').then(function(vegtablelist){
+                                const Price=vegtablelist.child('Rate').val();
+                                const totalstr = Price * weight;
+                                totalval = totalval + totalstr;
+                                console.log("weight",weight);
+                                var item = {
+                                    rate : totalstr
+                                    };
+                                 billingRef.child(userId).child(orderId).child('ItemList').child(historyitem.key).update(item);
+                            })
+                         })
+    
+                         if(totalval > 200){
+                            totalval = totalval;
+                         }else{
+                            totalval=totalval - 30 + 50;
+                         }
+    
+                         //admindata bill total and status  update
+                          var updatebill = {
+                            TotalRate : totalval,
+                            orderstate : "Approved"
+                            };
+    
+                           adminRef.child('Billing').child(orderId).child('Bill').update(updatebill);
+    
+                           adminRef.child('History').child(orderId).child('Bill').update(updatebill);
+    
+                           billingRef.child(userId).child(orderId).child('Bill').update(updatebill);
+                         
+                        })
+    
+                    }
+                  });
+            });
+        
+               var response = {
+                message : "updated successfully"
+                };
+        
+            res.send(response);
+        }catch(e){
+            console.log(e);
+        }
+    });
   
 
     server.put('/Approved', (req, res)=>{
