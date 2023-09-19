cb6fb8d0d58ecb5f9b4bd06ba955cbd134303d7e
manojthoffen <manojkumar.t@hoffensoft.com>
2023-09-19 13:09:25 +0530

    testing

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -142,7 +142,7 @@ server.use(cors());
             adminRef.child('Billing').once('value').then(function(snapshot) {
                 snapshot.forEach((snapshot) => {
     
-                     if(snapshot.child('Bill').child('orderstate').val() == "Pending"){
+                     if(snapshot.child('Bill').child('orderstate').val() == "Pending" && snapshot.child('Bill').child('Date').val() == datestr){
                           var orderId = snapshot.child('Bill').child('OrderID').val();
                           var userId = snapshot.child('Bill').child('userId').val();
     
