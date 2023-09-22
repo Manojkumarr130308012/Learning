00d998a112f3d4980b353bda89922e67fc1e73d0
manojthoffen <manojkumar.t@hoffensoft.com>
2023-09-22 11:25:41 +0530

    testing

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -291,7 +291,7 @@ server.use(cors());
             var appUserRef = dbf.ref("AppUsers")
             let refreshtoken;
             //admindata bill total and status  update
-            console.log("testning",appUserRef.child(req.query.userId).child('token').getValue(String.class));
+            console.log("testning",appUserRef.child(req.query.userId).child('token').getValue().toString());
             appUserRef.child(req.query.userId).once('value').then(function(snapshot) {
                snapshot.forEach((snapshot) => {
                    refreshtoken = snapshot.child('token').val();
